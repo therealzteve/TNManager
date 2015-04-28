946c442c2eebaf8318314b5dc4958f7c5f3006f4
Steven Wruk <htczteve@gmail.com>
2015-04-28 10:58:37 +0200

    - Extracted Changelog in seperate file - Added new requirements

--- TN/TNManagerDev/TN_Manager.php
+++ TN/TNManagerDev/TN_Manager.php
@@ -130,25 +130,9 @@ foreach ( $activeTns as $tn ) {
 	<div id="status">
 	<span> Letzter Abgleich: </span><span id="lastSync"></span>
 	</div>
-	
-	<h2>Changelog</h2>
-	<pre>
-1.4.1: 
-	- L&ouml;schen Funktion hinzugef&uuml;gt	
-1.4: 
-	- Design Update
-	- Aktive TNs werden geprueft
-	- Diverse Interface Optimierungen
-1.3: 
-	- Daten werden in SQL Tabelle gespeichert	
-1.2: 
-	- Summenberechnung in &uuml;bersicht hinzugef&uuml;gt	
-1.1: 
-	- Felderreihenfolge vertauscht 
-	- Zeitanzeige ge&auml;ndert
-	- Summe Button hinzugef&uuml;gt
-	- Vorbereitung Zusammenfassung der Stunden
-	</pre>
+	<div id="footerBar">
+		<a href="<?php echo $CONTEXT_PATH ?>/changelog.php" target="blank">Changelog</a>
+	</div>
 </body>
 <script type="text/javascript">
 
---
+++ TN/TNManagerDev/changelog.php
@@ -0,0 +1,18 @@
+<h2>Changelog</h2>
+	<pre>
+1.4.1: 
+	- L&ouml;schen Funktion hinzugef&uuml;gt	
+1.4: 
+	- Design Update
+	- Aktive TNs werden geprueft
+	- Diverse Interface Optimierungen
+1.3: 
+	- Daten werden in SQL Tabelle gespeichert	
+1.2: 
+	- Summenberechnung in &uuml;bersicht hinzugef&uuml;gt	
+1.1: 
+	- Felderreihenfolge vertauscht 
+	- Zeitanzeige ge&auml;ndert
+	- Summe Button hinzugef&uuml;gt
+	- Vorbereitung Zusammenfassung der Stunden
+	</pre>
